147d001ec0844ef0f33d6950ac506da8e45ae0d1
Max-55555 <maxvroemen04@gmail.com>
2020-01-29 21:37:03 -0800

    Made it so the prefix is "-" and we now have array of commands

--- node/index.js
+++ node/index.js
@@ -6,10 +6,19 @@ client.on('ready', () => {
   console.log(`Logged in!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('Pong');
+client.on('message', (m) => {
+  var content = m.content;
+  if (content.startsWith("-")) {
+    content = content.replace("-","")
+    command(content.split(" "), m);
   }
 });
+
+function command(args, m) {
+  m.channel.send(args.length);
+  for (i=0;i<args.length;i++) {
+    m.channel.send(args[i]);
+  }
+}
 
 client.login('NjcyMjU2ODg0MTc4Mjg4NjQ4.XjJaHw.IabX1HBSPniVrYMq673ROwQDrCc');
