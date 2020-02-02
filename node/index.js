2722ee7c69bde84a68c085b8c8ca1c02e5643591
Max-55555 <maxvroemen04@gmail.com>
2020-02-02 14:02:12 -0800

    How to parse with nums help

--- node/index.js
+++ node/index.js
@@ -23,19 +23,17 @@ function command(args, m) {
   }
   if (args[0] == 'wg') {
     console.log("Trying to acces WG API")
-    request.open('GET', 'https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=2162639654e42bd90a76343ad68b3c23&nation=USA&ship_id=3666786288');
+    request.open('GET', 'https://api.worldofwarships.eu/wows/encyclopedia/ships/?application_id=2162639654e42bd90a76343ad68b3c23&nation=USA&ship_id=4074682352');
     request.onreadystatechange = function() {
       console.log(this.status);
-      if (this.status == 200) {
+      if (this.status == 200 && this.readyState == 4) {
         var apidata = JSON.parse(this.responseText);
-        m.channel.send(apidata.status);
-        var metaa = apidata.meta;
-        m.channel.send(metaa.count);
-        m.channel.send(metaa.limit);
+        console.log(apidata.status);
+        console.log(apidata.meta.limit);
       }
     }
     request.send();
   }
 }
 
-client.login('ping me');
+client.login('kek');
